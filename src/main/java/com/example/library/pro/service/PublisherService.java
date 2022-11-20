bee2a56714439fd0cd2425401497c2b2caeb5065
jiuthree <2549927770@qq.com>
2022-11-20 17:44:12 +0800

    增加添加发布者相关接口

---
+++ src/main/java/com/example/library/pro/service/PublisherService.java
@@ -0,0 +1,43 @@
+package com.example.library.pro.service;
+
+import com.example.library.pro.dao.PublisherDao;
+import com.example.library.pro.exception.RequestException;
+import com.example.library.pro.module.Library;
+import com.example.library.pro.module.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PublisherService {
+    @Autowired
+    PublisherDao publisherDao;
+
+    public Publisher getById(Long id) {
+        Optional<Publisher> res = publisherDao.findById(id);
+        if (!res.isPresent()) {
+            throw new RequestException(HttpStatus.BAD_REQUEST, "该发布者不存在");
+        }
+        return res.get();
+    }
+
+    public List<Publisher> findAll() {
+        return publisherDao.findAll();
+    }
+
+    public Publisher add(Publisher publisher) {
+        return publisherDao.save(publisher);
+    }
+
+    public Void deleteById(Long id) {
+        if(!publisherDao.existsById(id)){
+            throw new RequestException(HttpStatus.BAD_REQUEST, "该发布者不存在");
+        }
+        publisherDao.deleteById(id);
+
+        return null;
+    }
+}
