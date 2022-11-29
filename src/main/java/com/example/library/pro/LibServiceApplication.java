f5f3b19adce118d305baeb19e4a3123feb8f4225
jiuthree <2549927770@qq.com>
2022-11-29 21:58:34 +0800

    修改jvm时间为北京时间

--- src/main/java/com/example/library/pro/LibServiceApplication.java
+++ src/main/java/com/example/library/pro/LibServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class LibServiceApplication {
@@ -13,4 +16,8 @@ public class LibServiceApplication {
         SpringApplication.run(LibServiceApplication.class, args);
     }
 
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+    }
 }
